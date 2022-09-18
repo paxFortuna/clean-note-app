179f3543686c24c4c0143e48642ca18f699639e3
fortuna <avidia38@gmail.com>
2022-09-18 18:10:08 +0900

    feat: notes_state in notes_view_model

---
+++ lib/presentation/notes/notes_state.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notes_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_NotesState _$$_NotesStateFromJson(Map<String, dynamic> json) =>
+    _$_NotesState(
+      notes: json['notes'] as List<dynamic>,
+    );
+
+Map<String, dynamic> _$$_NotesStateToJson(_$_NotesState instance) =>
+    <String, dynamic>{
+      'notes': instance.notes,
+    };
--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:clean_note_app/domain/repository/note_repository.dart';
 import 'package:clean_note_app/presentation/notes/notes_event.dart';
+import 'package:clean_note_app/presentation/notes/notes_state.dart';
 import 'package:flutter/foundation.dart';
 import '../../domain/model/note.dart';
 
@@ -10,8 +11,11 @@ class NoteViewModel with ChangeNotifier {
 
   NoteViewModel(this.repository);
 
-  List<Note> _notes = [];
-  UnmodifiableListView<Note> get note => UnmodifiableListView(_notes);
+  NotesState _state = NotesState(notes: []);
+  NotesState get state => _state;
+
+  // List<Note> _notes = [];
+  // UnmodifiableListView<Note> get note => UnmodifiableListView(_notes);
 
   Note? _recentlyDeleteNote;
 
@@ -19,13 +23,16 @@ class NoteViewModel with ChangeNotifier {
     event.when(
       loadNotes: _loadNotes,
       deleteNote: _deleteNote,
-      restoreNote: () {},
+      restoreNote: _restoreNote,
     );
   }
 
   Future<void> _loadNotes() async {
     List<Note> notes = await repository.getNotes();
-    _notes = notes;
+    //_notes = notes;
+    _state = state.copyWith(
+      notes: notes,
+    );
     notifyListeners();
   }
 

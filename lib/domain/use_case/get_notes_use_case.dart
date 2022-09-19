bdcd31617cd45180b1526f583b504862d5bc455c
fortuna <avidia38@gmail.com>
2022-09-19 19:46:09 +0900

    sort by enum & switch

--- lib/domain/use_case/get_notes_use_case.dart
+++ lib/domain/use_case/get_notes_use_case.dart
@@ -2,12 +2,55 @@ import 'package:clean_note_app/domain/repository/note_repository.dart';
 
 import '../model/note.dart';
 
+enum NoteOrder {
+  title, date, color,
+}
+
+enum OrderType {
+  ascending, descending,
+}
+
 class GetNotesUseCase {
   final NoteRepository repository;
 
   GetNotesUseCase(this.repository);
 
-  Future<List<Note>> call() async {
+  Future<List<Note>> call(NoteOrder noteOrder, OrderType orderType) async {
+    switch(noteOrder){
+      case NoteOrder.title:
+
+        switch(orderType){
+          case OrderType.ascending:
+            // TODO: Handle this case.
+            break;
+          case OrderType.descending:
+            // TODO: Handle this case.
+            break;
+        }
+        break;
+      case NoteOrder.date:
+        switch(orderType){
+          case OrderType.ascending:
+            // TODO: Handle this case.
+            break;
+          case OrderType.descending:
+            // TODO: Handle this case.
+            break;
+        }
+        break;
+
+      case NoteOrder.color:
+        switch(orderType){
+          case OrderType.ascending:
+            // TODO: Handle this case.
+            break;
+          case OrderType.descending:
+            // TODO: Handle this case.
+            break;
+        }
+        // TODO: Handle this case.
+        break;
+    }
     List<Note> notes = await repository.getNotes();
     notes.sort((a,b) => -a.timestamp.compareTo(b.timestamp));
 

93a8e4ed2754429d0ef05d6b97752de011861c45
fortuna <avidia38@gmail.com>
2022-09-19 20:53:45 +0900

    add: notes_event -- changeOrder(noteOrder)

--- lib/presentation/notes/notes_event.dart
+++ lib/presentation/notes/notes_event.dart
@@ -1,3 +1,4 @@
+import 'package:clean_note_app/domain/util/note_order.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../domain/model/note.dart';
@@ -9,4 +10,5 @@ abstract class NotesEvent with _$NotesEvent {
   const factory NotesEvent.loadNotes() = LoadNotes;
   const factory NotesEvent.deleteNote(Note note) = DeleteNote;
   const factory NotesEvent.restoreNote() = RestoreNote;
+  const factory NotesEvent.changeOrder(NoteOrder noteOrder) = ChangeOrder;
 }
